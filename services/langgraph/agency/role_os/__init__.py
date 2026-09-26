9280889cc8cccbb455fe88037602bdb1cde7365d
Isaac Bishop <44534547+eyeszik@users.noreply.github.com>
2026-09-25 18:31:30 -0700

    Expose Agent Mission Control RoleOS profile API

--- services/langgraph/agency/role_os/__init__.py
+++ services/langgraph/agency/role_os/__init__.py
@@ -17,6 +17,8 @@ __all__ = [
     "MissionWorkOrderAdapter", "MissionAdapterError", "CompiledMissionWorkOrders",
     "NextBestActionEngine", "NextActionError", "ProjectExecutionState",
     "ActionScore", "NextActionSelection",
+    "AgencyStageBinding", "AGENCY_STAGE_BINDINGS", "AGENCY_STAGE_IDS", "AGENCY_PHASE_MAP",
+    "build_agency_role_os_mission", "compile_agency_role_os_work_orders",
 ]
 
 from .amc_profile import (
